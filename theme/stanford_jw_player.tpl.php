fa29946e3da047a3f873512553abcc49d539b9fa
John Bickar <jbickar@stanford.edu>
2013-03-20 16:04:10 -0700

    Fix for HTML5 video that does not break streaming RTMP

--- theme/stanford_jw_player.tpl.php
+++ theme/stanford_jw_player.tpl.php
@@ -10,12 +10,24 @@
  * - $file_url: The url of the file to be played.
  * - $jw_player_inline_js_code: JSON data with configuration settings for the video player.
  * - $poster: URL to an image to be used for the poster (ie. preview image) for this video.
+ * - $streaming: boolean; 1 if this is a streaming video
  *
  * @see template_preprocess_jw_player()
  */
+$source = '<source src="' . $file_url . '"';
+if(isset($file_mime)){
+  $source .= ' type="' . $file_mime .'"';
+}
+$source .= ' />';
 ?>
 <div class="jwplayer-video">
   <video id="<?php print $html_id ?>" width="<?php print $width ?>" height="<?php print $height ?>" controls="controls" preload="none"<?php if(isset($poster)) : ?> poster="<?php print $poster ?>"<?php endif ?>>
+  <?php
+    if($streaming != 1){
+      print $source;
+    }
+  ?>
+  Your browser does not support this video type.
   </video>
 </div>
 <?php if(isset($jw_player_inline_js_code)): ?>
